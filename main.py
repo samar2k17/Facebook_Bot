d43041b701e8e5bead5f36a5d16bb661bd5c57c3
M M Samar <34966884+samar2k17@users.noreply.github.com>
2021-02-11 22:10:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import time
 
 from Face_Bot.Face_Bot import Face_Bot
 if __name__ == "__main__":
-    username = '9205502894'
-    password = 'samar@123'
+    username = input()
+    password = input()
     url = 'https://www.facebook.com'
     driver = 'D:/chromedriver.exe'
     Face_Bot(driver, url, username, password)
